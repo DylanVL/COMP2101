8bcf7120b6a06a1b0b3144ee13fbefe6c7d59ded
DylanVL <200352421@student.georgianc.on.ca>
2022-09-21 17:41:33 -0400

    Finished

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -3,9 +3,15 @@
 
 echo 'FQDN:'"$(hostname)"
 
-echo "Host Information:" 
+echo "Host Information:"
 
-hostnamectl status | head 
+hostnamectl status | head
 
 echo "IP Addresses:"
-ip a s $(ip a s | grep -w inet | awk '{print $2}')| awk '{print $2}' 
+ip a s | grep -wh inet | grep -wv 'inet 127' | awk '{print $2}'
+ip a s | grep -wh inet6 | awk '{print $2}'
+
+echo "Root Filesystem Status:"
+df -hT --type ext4
+
+exit
